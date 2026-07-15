1f888f93eefb2b1f3a8840a99b419c690750de93
DineshVmg <vmgdinesh@gmail.com>
2026-07-15 08:48:43 +0530

    feat : initializing NutriGenius application structure

--- nutrigenius/lib/main.dart
+++ nutrigenius/lib/main.dart
@@ -1,28 +1,7 @@
 import 'package:flutter/material.dart';
+import 'app/app.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(const NutriGeniusApp());
-}
-
-class NutriGeniusApp extends StatelessWidget {
-  const NutriGeniusApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'NutriGenius',
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('NutriGenius'),
-        ),
-        body: const Center(
-          child: Text(
-            'Welcome to NutriGenius',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ),
-    );
-  }
 }
